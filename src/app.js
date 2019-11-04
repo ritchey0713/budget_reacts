f980a08525f5fc98d6972508f27dccab64475614
Dj <ritchey0713@gmail.com>
2019-11-04 14:09:54 -0500

    test router

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
 import React from "react"
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import "normalize.css/normalize.css";
 import "./styles/styles.scss"
 
-const app = document.getElementById("app")
+const dashBoard = () => {
+  return (
+    <div>
+      this is the dashboard
+    </div>
+  )
+}
 
-ReactDOM.render(<p>Hello!</p>, app)
+const app = document.getElementById("app")
+const routes = (
+  <BrowserRouter>
+    <Route path="/" component={dashBoard}/>
+  </BrowserRouter>
+)
+
+
+ReactDOM.render(routes, app)
