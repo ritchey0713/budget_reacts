dfc76750c13515412f1e4af9fbd77d59b92e3962
Dj <ritchey0713@gmail.com>
2019-11-05 13:55:40 -0500

    add router file

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
+
+import Header from "../components/Header.js"
+import DashBoard from "../components/DashBoard"
+import Create from "../components/Create"
+import Edit from "../components/Edit"
+import Help from "../components/Help"
+import NotFound from "../components/NotFound"
+
+const appRouter = () => {
+  return (
+    <BrowserRouter>
+    <div>
+      <Header/>
+      <Switch>
+        <Route path="/" component={DashBoard} exact={true}/>
+        <Route path="/create" component={Create}/>
+        <Route path="/edit" component={Edit}/>
+        <Route path="/help" component={Help}/>
+        <Route  component={NotFound}/>
+      </Switch>
+    </div>  
+  </BrowserRouter>
+  )
+}
+
+
+export default appRouter
