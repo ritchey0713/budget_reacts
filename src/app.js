50b5f1590d96ba1b8b4d69b94d073d8a6980c23a
Dj <ritchey0713@gmail.com>
2019-11-05 13:56:24 -0500

    refacotr

--- src/app.js
+++ src/app.js
@@ -1,24 +1,11 @@
 import React from "react"
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route } from 'react-router-dom'
+import AppRouter from './routers/AppRouter'
 
 import "normalize.css/normalize.css";
 import "./styles/styles.scss"
 
-const dashBoard = () => {
-  return (
-    <div>
-      this is the dashboard
-    </div>
-  )
-}
 
 const app = document.getElementById("app")
-const routes = (
-  <BrowserRouter>
-    <Route path="/" component={dashBoard}/>
-  </BrowserRouter>
-)
 
-
-ReactDOM.render(routes, app)
+ReactDOM.render(<AppRouter/>, app)
