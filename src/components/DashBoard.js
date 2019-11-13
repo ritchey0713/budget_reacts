47e6f78f51ec176b8e24f4e9048f44a77b406ff3
Dj <ritchey0713@gmail.com>
2019-11-13 16:11:11 -0500

    add comp calls to dash

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,11 +1,18 @@
 import React from "react"
+import ExpenseList from "./ExpenseList"
+import ExpenseListFilters from "./ExpenseListFilters"
+import { connect } from 'react-redux'
 
 const dashBoard = () => {
   return (
     <div>
       this is the dashboard
+      <ExpenseListFilters/>
+      <ExpenseList />
     </div>
   )
 }
+
+
 
 export default dashBoard
