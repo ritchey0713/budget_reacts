1327a9f9cc934413316ffb4b2543adfac8e1455c
Dj <ritchey0713@gmail.com>
2019-11-14 16:17:18 -0500

    add connect

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,17 +1,20 @@
 import React from "react" 
 import ExpenseForm from "./ExpenseForm"
+import { connect } from 'react-redux'
+import { addExpense } from "../actions/expenses"
 
-const create = () => {
+const CreateExpense = (props) => {
   return (
     <div>
       <h1>Add new budget</h1>
       <ExpenseForm
         onSubmit={(expense) => {
           console.log(expense)
+          props.dispatch(addExpense(expense))
         }}
       />
     </div>
   )
 }
 
-export default create
+export default connect()(CreateExpense)
