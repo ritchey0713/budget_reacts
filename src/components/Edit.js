25c61fbad348a1f40d054279f81dc6cfb8f63502
Dj <ritchey0713@gmail.com>
2019-11-15 12:48:37 -0500

    add form to edit container

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,12 +1,31 @@
 import React from 'react'
+import ExpenseForm from "./ExpenseForm"
+import { connect } from 'react-redux'
+import { editExpense } from '../actions/expenses'
 
-const edit = (props) => {
-  console.log(props)
+const EditExpense = (props) => {
   return (
     <div>
       Editing expense with an id of {props.match.params.id}
+      <ExpenseForm 
+        expense={props.expense}
+        reduxOnSubmit={(expense) => {
+          props.dispatch(editExpense(props.match.params.id, expense))
+          props.history.push("/")
+        }
+          
+        }
+      />
     </div>
   )
 }
 
-export default edit
+const mapStateToProps = (state, ownProps) => {
+  return {
+    expense: state.expenses.find((expense) => {
+      return expense.id === ownProps.match.params.id
+    })
+  }
+}
+
+export default connect(mapStateToProps)(EditExpense)
