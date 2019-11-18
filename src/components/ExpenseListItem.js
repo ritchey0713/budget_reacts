cc380764028b36120a56c85d2ca42383bcf96778
Dj <ritchey0713@gmail.com>
2019-11-18 11:26:27 -0500

    remove comments

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,17 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { Link } from 'react-router-dom'
-// const ExpenseListItem = (props) => (
-//   <div>
-//     <h3>
-//       DESC: {props.expense.description}  
-//     </h3>
-
-//     <p>
-//       Created at: {props.expense.createdAt}  
-//     </p>
-//   </div>
-// )
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
