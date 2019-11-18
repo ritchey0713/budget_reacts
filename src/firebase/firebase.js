cba1f7e2aa72525f43133c0df6d629238ad46d01
Dj <ritchey0713@gmail.com>
2019-11-18 13:04:31 -0500

    remove badd firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,18 +1,26 @@
 import * as firebase from 'firebase'
+import firebaseConfig from "./firebaseSecrets"
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDcp7U9QwJDT5RJfyhT_9FdBVW5h1PwhoA",
-  authDomain: "fir-app-1c9bd.firebaseapp.com",
-  databaseURL: "https://fir-app-1c9bd.firebaseio.com",
-  projectId: "fir-app-1c9bd",
-  storageBucket: "fir-app-1c9bd.appspot.com",
-  messagingSenderId: "469509466056",
-  appId: "1:469509466056:web:172e7aa4801238a0ba42fd",
-  measurementId: "G-33BWNNXRT5"
-};
+
 
 firebase.initializeApp(firebaseConfig);
 
-firebase.database().ref().set({
-  name: "Dj Ritchey"
+const dataBase = firebase.database()
+
+dataBase.ref().set({
+  name: "Dj Ritchey",
+  age: 29,
+  isSingle: true,
+  location: {
+    country: "United States"
+  }
+})
+
+// dataBase.ref().set("this is more data")
+
+dataBase.ref("age").set(30)
+dataBase.ref('location/city').set("columbus")
+dataBase.ref("attributes").set({
+  height: 69,
+  weight: 170
 })
