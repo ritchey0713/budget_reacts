521d1a2f63e25c9985256bd74c4ebc4dcdeccec3
Dj <ritchey0713@gmail.com>
2019-11-20 12:13:31 -0500

    add promises

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,4 @@
-import * as firebase from 'firebase'
+import * as firebase from "firebase"
 import firebaseConfig from "./firebaseSecrets"
 
 
@@ -14,13 +14,34 @@ dataBase.ref().set({
   location: {
     country: "United States"
   }
+}).then(() => {
+  console.log("SAVED")
+}).catch((error) => {
+  console.log(error, "error")
+})
+
+dataBase.ref().update({
+  name: "Tom",
+  age: 55,
+  job: "Dev",
+  isSingle: null
 })
 
 // dataBase.ref().set("this is more data")
 
-dataBase.ref("age").set(30)
-dataBase.ref('location/city').set("columbus")
-dataBase.ref("attributes").set({
-  height: 69,
-  weight: 170
-})
+// dataBase.ref("age").set(30)
+// dataBase.ref('location/city').set("columbus")
+// dataBase.ref("attributes").set({
+//   height: 69,
+//   weight: 170
+// }).then(() => {
+//   console.log("NEW SAVE")
+// }).catch((error) => {
+//   console.log("ERROR", error)
+// })
+
+// const user = dataBase.ref("isSingle")
+// user.remove()
+//   .then(() => {
+//     console.log("removed")
+//   })
