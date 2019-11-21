a4bdeb8322bf9fe6fe7413712ccc9d9a65880fb4
Dj <ritchey0713@gmail.com>
2019-11-21 13:20:43 -0500

    change to class var

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,21 +1,29 @@
 import React from "react" 
 import ExpenseForm from "./ExpenseForm"
 import { connect } from 'react-redux'
-import { addExpense } from "../actions/expenses"
+import { addExpense, startAddExpense } from "../actions/expenses"
 
-const CreateExpense = (props) => {
-  return (
-    <div>
-      <h1>Add new budget</h1>
-      <ExpenseForm
-        reduxOnSubmit={(expense) => {
-          console.log(expense)
-          props.dispatch(addExpense(expense))
-          props.history.push("/")
-        }}
-      />
-    </div>
-  )
+export class CreateExpense extends React.Component {
+  reduxOnSubmit = (expense) => {
+    this.props.startAddExpense(expense)
+    this.props.history.push("/")
+  }
+  render(){
+    return (
+      <div>
+        <h1>Add new budget</h1>
+        <ExpenseForm
+          reduxOnSubmit={this.reduxOnSubmit}
+        />
+      </div>
+    )
+  } 
 }
 
-export default connect()(CreateExpense)
+const mapDispatchToProps = (dispatch) => {
+  return {
+    startAddExpense: (expense) => dispatch(startAddExpense(expense))
+  }
+}
+
+export default connect(undefined, mapDispatchToProps)(CreateExpense)
