f5d8ca115ce69caa96cbbbb4fbdf35fcdc9bfe15
Dj <ritchey0713@gmail.com>
2019-11-21 14:59:31 -0500

    create login func

---
+++ src/components/LoginPage.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Login = () => {
+  return (
+    <div>
+      Login page
+      <button>
+        Login here!
+      </button>
+    </div>
+  )
+}
+
+export default Login
