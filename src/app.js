cc8012c2b1766c74cac28b38b35c99925bbe1f75
Dj <ritchey0713@gmail.com>
2019-11-25 12:00:29 -0500

    remove header

--- src/app.js
+++ src/app.js
@@ -4,9 +4,10 @@ import { Provider } from "react-redux"
 import AppRouter, { history } from './routers/AppRouter'
 import configureStore from "./store/configureStore"
 import { startSetExpenses } from "./actions/expenses"
-import {  setEndDate, setStartDate, setTextFilter, sortByAmount, sortByDate } from "./actions/filters"
 import getVisibleExpenses from "./selectors/expenses"
+import { login, logout } from "./actions/auth"
 import { firebase } from "./firebase/firebase"
+
 
 import "normalize.css/normalize.css";
 import "./styles/styles.scss"
@@ -20,6 +21,15 @@ const store = configureStore();
 
 const app = document.getElementById("app")
 
+let hasRendered = false;
+
+const renderApp = () => {
+  if(!hasRendered){
+    ReactDOM.render(jsx, app)
+    hasRendered = true
+  }
+}
+
 const jsx = (
   <Provider store={store}>
     <AppRouter />
@@ -28,16 +38,20 @@ const jsx = (
 
 ReactDOM.render(<p>loading...</p>, app)
 
-store.dispatch(startSetExpenses())
-  .then(()=> {
-    ReactDOM.render(jsx, app)
-  })
-
 firebase.auth().onAuthStateChanged((user) => {
   if(user){
-    // console.log("logged in")
+    store.dispatch(login(user.uid))
+    store.dispatch(startSetExpenses())
+      .then(()=> {
+        renderApp()
+    })
+    if (history.location.pathname === "/") {
+      history.push("/dashboard")
+    }
   }
   else {
+    store.dispatch(logout())
+    renderApp()
     history.push("/")
   }
 })
