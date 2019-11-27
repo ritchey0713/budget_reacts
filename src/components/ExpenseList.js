69c3d16edc5ed62064f4503ad3e8d9c507de7681
Dj <ritchey0713@gmail.com>
2019-11-27 12:13:13 -0500

    move summary to top

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,11 +3,9 @@ import PropTypes from "prop-types"
 import { connect } from 'react-redux'
 import ExpenseListItem from "./ExpenseListItem"
 import selectExpenses from "../selectors/expenses"
-import ExpenseSummary from "./ExpenseSummary"
 const ExpenseList = (props) => {
   return (
     <div>
-      <ExpenseSummary />
       {props.expenses.map((expense) => {
         return (
           <ExpenseListItem
