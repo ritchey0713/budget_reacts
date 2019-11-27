7f4606f02c0662f23996eb7d5098ab8f396164f9
Dj <ritchey0713@gmail.com>
2019-11-27 11:51:00 -0500

    add summary

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,11 +3,11 @@ import PropTypes from "prop-types"
 import { connect } from 'react-redux'
 import ExpenseListItem from "./ExpenseListItem"
 import selectExpenses from "../selectors/expenses"
-
+import ExpenseSummary from "./ExpenseSummary"
 const ExpenseList = (props) => {
   return (
     <div>
-      <h1>expense list</h1>
+      <ExpenseSummary />
       {props.expenses.map((expense) => {
         return (
           <ExpenseListItem
