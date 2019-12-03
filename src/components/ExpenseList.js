81251a7833ff2dccdd4ff26eb706d1f589ea9f1d
Dj <ritchey0713@gmail.com>
2019-12-03 12:34:09 -0500

    add new divs

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -5,8 +5,14 @@ import ExpenseListItem from "./ExpenseListItem"
 import selectExpenses from "../selectors/expenses"
 const ExpenseList = (props) => {
   return (
-    <div>
-      {props.expenses.map((expense) => {
+    <div className="content-container">
+      <div className="list-header">
+        <div className="show-for-mobile">Expenses</div>
+        <div className="show-for-desktop">Expense</div>
+        <div className="show-for-desktop">Amount</div>
+      </div>
+      {props.expenses.length === 0 ? (<p>No expenses!</p>) : (
+        props.expenses.map((expense) => {
         return (
           <ExpenseListItem
             {...expense}
@@ -14,7 +20,8 @@ const ExpenseList = (props) => {
           />
          
         ) 
-      })}
+      }))}
+      
     </div>
   )
 }
