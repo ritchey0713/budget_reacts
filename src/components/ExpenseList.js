52a426297e75fbb038b0d09a6e03e3db90c3298c
Dj <ritchey0713@gmail.com>
2019-12-03 13:52:25 -0500

    add new divs

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -11,17 +11,22 @@ const ExpenseList = (props) => {
         <div className="show-for-desktop">Expense</div>
         <div className="show-for-desktop">Amount</div>
       </div>
-      {props.expenses.length === 0 ? (<p>No expenses!</p>) : (
-        props.expenses.map((expense) => {
-        return (
-          <ExpenseListItem
-            {...expense}
-            key={expense.id || "test-key"}
-          />
-         
-        ) 
-      }))}
-      
+      <div className="list-body">
+        {props.expenses.length === 0 ? (
+          <div className="list-item list-item--message">
+            <span>No Expenses</span>
+          </div>
+          ) : (
+            props.expenses.map((expense) => {
+              return (
+                <ExpenseListItem
+                  {...expense}
+                  key={expense.id || "test-key"}
+                />
+          
+          ) 
+        }))}
+      </div>
     </div>
   )
 }
