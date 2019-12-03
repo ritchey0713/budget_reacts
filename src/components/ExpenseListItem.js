e689fb706399883c06063be5e5d95bac4aa49363
Dj <ritchey0713@gmail.com>
2019-12-03 12:34:02 -0500

    reformat

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,22 +1,20 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { Link } from 'react-router-dom'
+import moment from 'moment'
+import numeral from 'numeral'
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
-  <div>
-    <h3>
-      <Link to={`/edit/${id}`}>DESC: {description}  </Link>
-    </h3>
+    <Link className="list-item" to={`/edit/${id}`}>
+      <div>
+        <h3>{description}</h3>
+        <span>{moment(createdAt).format("MMM Do, YYYY")}</span>
+      </div>
+      <h3>
+        {numeral(amount/100).format('$0.00')}  
+      </h3>
 
-    <p>
-      Amount: {amount}  
-    </p>
-
-    <p>
-      Created at: {createdAt}  
-    </p>
-
-  </div>
+    </Link>
 )
 
 ExpenseListItem.propTypes = {
