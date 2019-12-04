22b41aa26e53eba8043fb2c083a034a5f4b5bf93
Dj <ritchey0713@gmail.com>
2019-12-04 11:32:29 -0500

    .

--- src/app.js
+++ src/app.js
@@ -39,20 +39,20 @@ const jsx = (
 
 ReactDOM.render(<LoadingPage />, app)
 
-// firebase.auth().onAuthStateChanged((user) => {
-//   if(user){
-//     store.dispatch(login(user.uid))
-//     store.dispatch(startSetExpenses())
-//       .then(()=> {
-//         renderApp()
-//     })
-//     if (history.location.pathname === "/") {
-//       history.push("/dashboard")
-//     }
-//   }
-//   else {
-//     store.dispatch(logout())
-//     renderApp()
-//     history.push("/")
-//   }
-// })
+firebase.auth().onAuthStateChanged((user) => {
+  if(user){
+    store.dispatch(login(user.uid))
+    store.dispatch(startSetExpenses())
+      .then(()=> {
+        renderApp()
+    })
+    if (history.location.pathname === "/") {
+      history.push("/dashboard")
+    }
+  }
+  else {
+    store.dispatch(logout())
+    renderApp()
+    history.push("/")
+  }
+})
