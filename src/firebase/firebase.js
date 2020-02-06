91cb75843375989915c0f864f128616a55302f4a
ritchey0713 <ritchey0713@gmail.com>
2020-02-06 00:49:23 -0500

    ,

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,7 +4,7 @@ import * as firebase from "firebase"
 firebase.initializeApp({
   apiKey: process.env.APIKEY,
   authDomain: process.env.AUTHDOMAIN,
-  databaseURL: process.env.DATABASEURL,
+  databaseURL: `${process.env.DATABASEURL}`,
   projectId: process.env.PROJECTID,
   storageBucket: process.env.STORAGEBUCKET,
   messagingSenderId: process.env.MESSAGINGSENDERID,
