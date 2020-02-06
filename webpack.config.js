75eaba251cfaffa12eceb0effc748b78b1ef9974
ritchey0713 <ritchey0713@gmail.com>
2020-02-05 23:28:14 -0500

    fix loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 //entry -> output 
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = (env) => {
 
   const isProduction = env === "production"
+  const CSSExtract = new ExtractTextPlugin('styles.css')
 
   return {
     entry: ["babel-polyfill", "./src/app.js"],
@@ -18,13 +20,17 @@ module.exports = (env) => {
       exclude: /node_modules/
     }, {
       test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
+      use: CSSExtract.extract({
+        use: [
+          'css-loader',
+          'sass-loader'
+        ]
+      })
     }]
   },
+  plugins: [
+    CSSExtract
+  ],
   devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
   devServer: {
     contentBase: path.join(__dirname, 'public'),
