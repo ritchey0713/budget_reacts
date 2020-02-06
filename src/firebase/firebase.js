23c000d2d18393b2237bbb17ee24fce768c095ba
ritchey0713 <ritchey0713@gmail.com>
2020-02-06 00:57:21 -0500

    ...

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,19 @@
 import * as firebase from "firebase"
 // import firebaseConfig from "./firebaseSecrets"
 
+const config = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID
+}
+
+console.log(config)
+
 firebase.initializeApp({
   // apiKey: process.env.APIKEY,
   // authDomain: process.env.AUTHDOMAIN,
