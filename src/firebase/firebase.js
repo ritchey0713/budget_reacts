86aa10a8267c0b1b9e32b58d7d0f8d5b0cb29b02
ritchey0713 <ritchey0713@gmail.com>
2020-02-06 00:08:54 -0500

    fix env issue

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,14 +4,14 @@ import * as firebase from "firebase"
 
 
 firebase.initializeApp({
-    apiKey: ENV['APIKEY'],
-  authDomain: ENV['AUTHDOMAIN'],
-  databaseURL: ENV['DATABASEURL'],
-  projectId: ENV["PROJECTID"],
-  storageBucket: ENV['STORAGEBUCKET'],
-  messagingSenderId: ENV['MESSAGINGSENDERID'],
-  appId: ENV['APPID'],
-  measurementId: ENV['MEASUREMENTID']
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID
 });
 
 
