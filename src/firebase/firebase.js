eaa24e5961cbd7fddf1d72f0de21b7192710da2b
ritchey0713 <ritchey0713@gmail.com>
2020-02-06 00:54:42 -0500

    ?????

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,21 @@ import * as firebase from "firebase"
 // import firebaseConfig from "./firebaseSecrets"
 
 firebase.initializeApp({
-  apiKey: process.env.APIKEY,
+  // apiKey: process.env.APIKEY,
+  // authDomain: process.env.AUTHDOMAIN,
+  // databaseURL: process.env.DATABASEURL,
+  // projectId: process.env.PROJECTID,
+  // storageBucket: process.env.STORAGEBUCKET,
+  // messagingSenderId: process.env.MESSAGINGSENDERID,
+  // appId: process.env.APPID,
+  // measurementId: process.env.MEASUREMENTID
+    apiKey: process.env.APIKEY,
   authDomain: process.env.AUTHDOMAIN,
-  databaseURL: `${process.env.DATABASEURL}`,
+  databaseURL: process.env.DATABASEURL,
   projectId: process.env.PROJECTID,
   storageBucket: process.env.STORAGEBUCKET,
   messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APPID,
-  measurementId: process.env.MEASUREMENTID
+  
 });
 
 // local init
