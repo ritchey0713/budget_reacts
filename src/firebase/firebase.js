895d77382d4ae22404b7ae867a8b0c415a238452
ritchey0713 <ritchey0713@gmail.com>
2020-02-06 00:44:56 -0500

    ,

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,14 +4,13 @@ import * as firebase from "firebase"
 firebase.initializeApp({
   apiKey: process.env.APIKEY,
   authDomain: process.env.AUTHDOMAIN,
-  databaseURL: process.env.DATA_BASE_URL,
+  databaseURL: process.env.DATABASEURL,
   projectId: process.env.PROJECTID,
   storageBucket: process.env.STORAGEBUCKET,
   messagingSenderId: process.env.MESSAGINGSENDERID,
   appId: process.env.APPID,
   measurementId: process.env.MEASUREMENTID
 });
-
 
 // local init
 // firebase.initializeApp(firebaseConfig);  
