2a78a32c67050df050d66f78fb0ee341b2dc7448
Dj <ritchey0713@gmail.com>
2020-03-07 12:52:41 -0500

    add filter tests

---
+++ src/tests/actions/filters.test.js
@@ -0,0 +1,57 @@
+import moment from 'moment'
+
+import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from "../../actions/filters"
+
+test("Should generate set start date action object", () => {
+  const action = setStartDate(moment(0))
+
+  expect(action).toEqual({
+    type: "SET_START_DATE",
+    date: moment(0)
+  })
+})
+
+test("Should generate set end date action object", () => {
+  const action = setEndDate(moment(0))
+
+  expect(action).toEqual({
+    type: "SET_END_DATE",
+    date: moment(0)
+  })
+})
+
+test("Should generate set text action object with provided text", () => {
+  const action = setTextFilter("Hello world!")
+
+  expect(action).toEqual({
+    type: "SET_TEXT_FILTER",
+    text: "Hello world!"
+  })
+})
+
+test("Should generate set text action object with default arg", () => {
+  const action = setTextFilter()
+
+  expect(action).toEqual({
+    type: "SET_TEXT_FILTER",
+    text: ""
+  })
+})
+
+test("Should generate sort by amount action object with provided text", () => {
+  const action = sortByAmount()
+
+  expect(action).toEqual({
+    type: "SET_SORT_BY_AMOUNT",
+    text: "amount"
+  })
+})
+
+test("Should generate sort by date action object with provided text", () => {
+  const action = sortByDate()
+
+  expect(action).toEqual({
+    type: "SET_SORT_BY_DATE",
+    text: "date"
+  })
+})
