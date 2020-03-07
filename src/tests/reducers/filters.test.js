154b59aa50eff1ff8b7520b102fe71c4ed6fed7e
Dj <ritchey0713@gmail.com>
2020-03-07 16:14:46 -0500

    add filter reducers tests

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -19,5 +19,41 @@ test("should set sortBy to amount", () => {
 })
 
 test("should set sortBy to date", () => {
-  
+  const currentState = {
+    text: "",
+    startDate: undefined,
+    endDate: undefined,
+    sortBy: "amount"
+  }
+  const action = {
+    type: "SET_SORT_BY_DATE",
+    text: "date"
+  }
+  const state = filtersReducer(currentState, action)
+
+  expect(state.sortBy).toBe("date")
+})
+
+test("should set text filter value", () => {
+  const text = "Hello!"
+  const action = {type: "SET_TEXT_FILTER", text}
+  const state = filtersReducer(undefined, action)
+
+  expect(state.text).toBe(text)
+})
+
+test("Should set startDate filter", () => {
+  const startDate = moment(0)
+  const action = {type: "SET_START_DATE", date: startDate}
+  const state = filtersReducer(undefined, action)
+
+  expect(state.startDate).toBe(startDate)
+})
+
+test("Should set endDate filter", () => {
+  const endDate = moment(1000)
+  const action = {type: "SET_END_DATE", date: endDate}
+  const state = filtersReducer(undefined, action)
+
+  expect(state.endDate).toBe(endDate)
 })
