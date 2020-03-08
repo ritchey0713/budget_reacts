bfca9995133b5d9446e865111693f964daf899af
Dj <ritchey0713@gmail.com>
2020-03-07 21:41:03 -0500

    add basic test

---
+++ src/tests/components/Header.test.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ShallowRenderer from "react-test-renderer/shallow"
+
+import {Header} from "../../components/Header"
+
+
+test("should render header component", () => {
+  const renderer = new ShallowRenderer()
+  renderer.render(<Header />)
+  expect(renderer.getRenderOutput()).toMatchSnapshot()
+
+})
