b89023f40e504cab0d6cf137215519a6fc0db3f4
Dj <ritchey0713@gmail.com>
2020-03-08 18:55:43 -0400

    add before each block

--- src/tests/components/Create.test.js
+++ src/tests/components/Create.test.js
@@ -3,17 +3,20 @@ import { shallow } from "enzyme"
 import { CreateExpense } from "../../components/Create.js"
 import expenses from "../fixtures/expenses"
 
+let onSubmit, history, wrapper;
+
+beforeEach(() => {
+  onSubmit = jest.fn()
+  history = {push: jest.fn()}
+  wrapper = shallow(<CreateExpense startAddExpense={onSubmit} history={history}/>)
+})
+
 test("should render create expense page", () => {
-  const onSubmit = jest.fn()
-  const history = {push: jest.fn()}
-  const wrapper = shallow(<CreateExpense startAddExpense={onSubmit} history={history}/>)
+
   expect(wrapper).toMatchSnapshot()
 })
 
 test("should handle onSubmit", () => {
-  const onSubmit = jest.fn()
-  const history = {push: jest.fn()}
-  const wrapper = shallow(<CreateExpense startAddExpense={onSubmit} history={history}/>)
   wrapper.find("ExpenseForm").prop("reduxOnSubmit")(expenses[0])
 
   expect(history.push).toHaveBeenLastCalledWith("/")
