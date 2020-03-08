d9bac9aa68bb20a308b7ebd5ea95d29091b50582
Dj <ritchey0713@gmail.com>
2020-03-08 00:27:04 -0400

    export unconnected component

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 import ExpenseListItem from "./ExpenseListItem"
 import selectExpenses from "../selectors/expenses"
 
-const ExpenseList = (props) => {
+export const ExpenseList = (props) => {
   return (
     <div className="content-container">
       <div className="list-header">
