70f5f7f73b0c3e09b8b30e1d27c5b2950ebcef4d
Dj <ritchey0713@gmail.com>
2020-03-08 20:45:25 -0400

    .

---
+++ src/tests/fixtures/filters.js
@@ -0,0 +1,15 @@
+import moment from 'moment'
+
+const filters = {
+  test: "",
+  sortBy: "date",
+  startDate: undefined,
+  endDate: undefined
+}
+
+const altFilters = {
+  test: "e",
+  sortBy: "amount",
+  startDate: undefined,
+  endDate: undefined
+}
